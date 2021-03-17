c80a6f18a15a9df5b413d63e5b294aad820157f1
Atalay <atalaykeskin@gmail.com>
2021-03-17 09:54:13 +0300

    RouterModule kullanılmaya başlandı.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarComponent } from './components/car/car.component';
+import { CustomerComponent } from './components/customer/customer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: CarComponent },
+  { path: 'customers', component: CustomerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
